dd23a251a2e279af1f2bbb2fcc5d84aea5909a28
irinakosenkova <Lrinakos2002@gmail.com>
2025-03-05 16:04:52 +0200

    Add turbine library to test channel. Fix current unit tests by verifying methods used in viewmodel init block. In IncomeModel and ExpanseModel remove default value of current time. Set current time for these models in viewmodel via time provider. Remove redundant dispatchers switch (in data store and room methods). Cover TransactionViewModel with unit tests.

--- app/src/test/java/com/example/nebulatest/TransactionViewModelUnitTest.kt
+++ app/src/test/java/com/example/nebulatest/TransactionViewModelUnitTest.kt
@@ -3,16 +3,15 @@ package com.example.nebulatest
 import app.cash.turbine.test
 import com.example.nebulatest.core.TimeProvider
 import com.example.nebulatest.features.balance.data.BalanceLocalDataSource
-import com.example.nebulatest.features.exchange.rate.domain.GetExchangeRateUseCase
 import com.example.nebulatest.features.transaction.data.local.TransactionLocalRepository
-import com.example.nebulatest.features.transaction.model.IncomeModel
-import com.example.nebulatest.ui.screens.home.HomeEvents
-import com.example.nebulatest.ui.screens.home.HomeViewModel
+import com.example.nebulatest.features.transaction.model.ExpanseModel
+import com.example.nebulatest.features.transaction.model.TransactionCategory
+import com.example.nebulatest.ui.screens.transaction.TransactionEvents
+import com.example.nebulatest.ui.screens.transaction.TransactionViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.emptyFlow
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
@@ -21,81 +20,82 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertNotNull
 import org.mockito.Mockito
 import org.mockito.Mockito.mock
+import org.mockito.MockitoAnnotations
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
 @OptIn(ExperimentalCoroutinesApi::class)
-class HomeViewModelUnitTest {
+class TransactionViewModelUnitTest {
     private val currentTime = 12L
 
-    private val getExchangeRateUseCase: GetExchangeRateUseCase = mock()
+    private lateinit var viewModel: TransactionViewModel
+
     private val transactionLocalRepository: TransactionLocalRepository = mock()
     private val balanceLocalDataSource: BalanceLocalDataSource = mock()
     private val timeProvider: TimeProvider = mock()
 
-    private lateinit var viewModel: HomeViewModel
     private val testDispatcher = StandardTestDispatcher()
 
     @BeforeEach
-    fun setup() = runTest(testDispatcher) {
+    fun setup() = runTest {
+        MockitoAnnotations.openMocks(this)
 
         Dispatchers.setMain(testDispatcher)
 
-        whenever(getExchangeRateUseCase.invoke()).thenReturn(Result.success(8432423423.633265))
-        whenever(balanceLocalDataSource.getBalance()).thenReturn(flowOf(1.4636546))
-        whenever(transactionLocalRepository.getTransactionsPaged()).thenReturn(emptyFlow())
         whenever(timeProvider.getCurrentTimeMillis()).thenReturn(currentTime)
 
-        viewModel = HomeViewModel(
-            getExchangeRateUseCase,
+        viewModel = TransactionViewModel(
             transactionLocalRepository,
             balanceLocalDataSource,
             timeProvider
         )
 
+        verifyNoMoreInteractions()
+    }
+
+    @Test
+    fun `add expanse and amount is not null test`() = runTest(UnconfinedTestDispatcher()) {
+        val amountText = "0.23"
+        val amount = 0.23
+
+        viewModel.addExpanse(amountText)
+
         advanceUntilIdle()
 
-        verify(balanceLocalDataSource).getBalance()
-        verify(getExchangeRateUseCase).invoke()
-        verify(transactionLocalRepository).getTransactionsPaged()
+        assertNotNull(amount)
+        verify(transactionLocalRepository).addExpense(
+            ExpanseModel(amount, viewModel.uiState.value.selectedCategory, currentTime)
+        )
+        verify(balanceLocalDataSource).updateBalance(-amount)
 
         verifyNoMoreInteractions()
     }
 
     @Test
-    fun `addIncome and income is null test`() = runTest {
-        val textIncome = "0,23,66"
+    fun `add expanse and amount is null test`() = runTest {
+        val amountText = "0,23,66"
 
         viewModel.events.test {
-            viewModel.addIncome(textIncome)
-            assertEquals(HomeEvents.ShowErrorSnackbar, awaitItem())
+            viewModel.addExpanse(amountText)
+            assertEquals(TransactionEvents.ShowErrorSnackbar, awaitItem())
         }
 
         verifyNoMoreInteractions()
     }
 
     @Test
-    fun `addIncome and income is not null test`() = runTest {
-        val textIncome = "0.23"
-        val income = 0.23
-
-        viewModel.addIncome(textIncome)
-
-        advanceUntilIdle()
-
-        verify(transactionLocalRepository).addIncome(IncomeModel(income, currentTime))
-        verify(balanceLocalDataSource).updateBalance(income)
+    fun `set category test`() {
+        viewModel.selectCategory(TransactionCategory.TAXI)
+        assertEquals(viewModel.uiState.value.selectedCategory, TransactionCategory.TAXI)
 
         verifyNoMoreInteractions()
     }
 
-
     private fun verifyNoMoreInteractions() {
-        Mockito.verifyNoMoreInteractions(
-            transactionLocalRepository, balanceLocalDataSource, getExchangeRateUseCase
-        )
+        Mockito.verifyNoMoreInteractions(transactionLocalRepository, balanceLocalDataSource)
     }
 
     @AfterEach
@@ -103,4 +103,3 @@ class HomeViewModelUnitTest {
         Dispatchers.resetMain()
     }
 }
-
